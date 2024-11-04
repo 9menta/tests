97167c11b853c2b47a94a0a4ed148d4ebfb7f5c7
9Menta <149399666+9menta@users.noreply.github.com>
2024-11-04 01:31:49 -0300

    Update notify.lua

--- notify.lua
+++ notify.lua
@@ -1,16 +1,13 @@
 local notification = loadstring(game:HttpGet('https://raw.githubusercontent.com/9menta/tests/refs/heads/main/notification.lua'))()
     notification({
-        Title = 'Cappuccino',
-        Text = 'very cool notification for gaming',
-        Image = 'rbxassetid://6353325673',    --optional
+        Title = 'Elixir Client',
+        Text = 'Thx for using Elixir Client',
+        Image = 'rbxassetid://72671288986713',    --optional
         Options = {                           --optional 
-            'Yes',
-            'No',
-            'Cheese',
-            'Cancel',
+            'No Problem'
         },
         CloseOnCallback = true,               --optional
-        Duration = 10,                        --optional
+        Duration = 20,                        --optional
         Callback = function(o)
             print('Option '..o..' was chosen')
         end,
